a89029bafd6fcc3a5bda6b81f18079efcff6edfa
Alberto0150 <albertosanjaya2@gmail.com>
2020-12-17 17:36:37 +0700

    fixing final + css

--- index.php
+++ index.php
@@ -5,29 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kuis PWEB</title>
+    <?php
+    include('extend.php');
+    ?>
 </head>
 <body>
-    <thread>
-    <tr>
-        <th>NAMA</th>
-        <th>WAKTU</th>
-    </tr>
-    </thread>
-    <tbody>
-    <?php
-        include('dbconnection.php');
+    <div class="container-fluid">
+        <table class="table table-striped">
+        <thread>
+        <tr>
+            <th>NAMA</th>
+            <th>WAKTU</th>
+        </tr>
+        </thread>
+        <tbody>
+        <?php
+            include('dbconnection.php');
 
-        $log = $db->real_escape_string('SELECT * FROM upload');
-        $query = $db->query($log);
-        while($hasil =$query->fetch_assoc()):
-    ?>
-    <tr>
-        <td><?= $hasil['USERNAME'] ?></td>
-        <td><?= $hasil['WAKTU'] ?></td>
-    </tr>
-    <?php
-        endwhile;
-    ?>
-    </tbody>
+            $log = $db->real_escape_string('SELECT * FROM upload');
+            $query = $db->query($log);
+            while($hasil =$query->fetch_assoc()):
+        ?>
+        <tr>
+            <td><?= $hasil['USERNAME'] ?></td>
+            <td><?= $hasil['WAKTU'] ?></td>
+        </tr>
+        <?php
+            endwhile;
+        ?>
+        </tbody>
+        </table>
+    </div>
 </body>
 </html>
